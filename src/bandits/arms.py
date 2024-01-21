cc7146a36f84c59e73f660816ba102530579c111
Demir Tonchev <tonchev.demir@gmail.com>
2024-01-21 23:43:10 +0200

    initial refactor of contextual arms added arnwrapper

--- src/bandits/arms.py
+++ src/bandits/arms.py
@@ -1,7 +1,5 @@
 from typing import Callable, Optional, Protocol, TypeAlias
 import numpy as np
-from functools import partial
-from numpy.random import default_rng
 from bandits.base import (bernuolli,
                           sigmoid,
                           check_random_state,
@@ -23,7 +21,7 @@ class BaseArm:
         raise NotImplementedError("method pull not implemented")
     
     def __str__(self) -> str:
-        return self.__class__.__qualname__
+        return f"<{self.__class__.__qualname__}>"
         
     def __repr__(self) -> str:
         return self.__class__.__qualname__
@@ -36,12 +34,9 @@ class ConstantArm(BaseArm):
 
         self.mean = constant
 
-    def pull(self):
+    def pull(self, size=None):
         return self.mean
     
-    def __repr__(self) -> str:
-        return super().__repr__()
-
 
 class BernulliArm(BaseArm):
     """ Simple arm with Bernoulli distribution support {0,1}"""
@@ -49,7 +44,6 @@ class BernulliArm(BaseArm):
     def __init__(self, p: float, seed=None):
         super().__init__(seed=seed)
 
-        assert 0 <= p <= 1, 'Bernulli parameter must be in [0,1]'
         self.mean = p
 
     def pull(self, size=None):
@@ -93,16 +87,26 @@ class ContextualBaseArm:
     def __str__(self) -> str:
         return self.__class__.__qualname__
 
-class _BaseArmWrapper:
-    def __init__(self, wrapped: BaseArm) -> None:
-        self.wrapped = wrapped
+class BaseArmWrapper:
+    def __init__(self, wrapped: BaseArm, transform_fn = lambda x:x) -> None:
+        self._wrapped = wrapped
+        self.transform_fn = transform_fn
+    
+    @property
+    def mean(self):
+        return self._wrapped.mean
+    
+    @mean.setter
+    def mean(self, new_mean):
+        self._wrapped.mean = new_mean
     
     def pull(self, new_mean, size=None):
-        self.wrapped.mean = new_mean
-        return self.wrapped.pull(size=size)
+        # self.wrapped.mean = self.transform_fn(new_mean)
+        self._wrapped.mean = self.transform_fn(new_mean)
+        return self._wrapped.pull(size=size)
     
     def __repr__(self) -> str:
-        return self.wrapped.__class__.__qualname__
+        return f"<{self.__class__.__qualname__}{self._wrapped}>"
         
 
 Vector1d: TypeAlias = list[float] | np.ndarray
@@ -114,7 +118,7 @@ class SimpleLinearArm(ContextualBaseArm):
 
     based on A Contextual-Bandit Approach to Personalized News Article Recommendation (https://arxiv.org/pdf/1003.0146.pdf)
 
-    param: theta - real coff vector (/theta* in the paper)
+    param: theta - real coff vector (\theta* in the paper)
     param: base_arm - the generator arm for the reward distribution. Default is ConstantArm, this would produce the same reward given the 
            context R_t | theta o context_t = theta o context_t . i.e. the reward is the dot product. But 
     param: seed - the seed object from numpy (or torch soon...)
@@ -124,9 +128,9 @@ class SimpleLinearArm(ContextualBaseArm):
         super().__init__(seed=seed)
         
         self.theta = np.asarray(theta)
-        self.base_arm = _BaseArmWrapper( ConstantArm(None) if base_arm is None else base_arm )
+        self.base_arm = BaseArmWrapper( ConstantArm(None) if base_arm is None else base_arm )
 
-        # self.dimension = len(theta)
+        self.dimension = self.theta.shape[0]
         self.current_mean = None
 
 
@@ -152,26 +156,24 @@ class SimpleLinearArm(ContextualBaseArm):
         return f"{self.__class__.__name__}({self.__dict__['theta'].tolist()})"
 
 class BernulliLinearArm(SimpleLinearArm):
-    """Class for generating bernuli rewards with sigmiod(real_theta dot context)
+    """Class for generating bernuli rewards with sigmoid(real_theta dot context)
     real reward probabilty depends on the context vector and some real Theta
     E[r_t | theta o context_t ]
 
     """
 
-    def __init__(self, theta: list | np.ndarray,
-                 norm_strategy: Optional[str] = None,
-                 seed: int | None = None,
-                 ):
-        super().__init__(theta, norm_strategy)
-        self.rng = default_rng(seed)
+    def __init__(self, theta: Vector1d, seed=None):
+        super().__init__(theta, None, seed=seed)
+        self.base_arm = BaseArmWrapper( BernulliArm(None), transform_fn=sigmoid)
 
 
-    def pull(self, context_vector):
+    def pull(self, context_vector: Vector1d, size=None):
         """generetate random reward with fixed p
         """
-        context_vector = np.array(context_vector)
-        self.current_mean = sigmoid(np.dot(self.theta, context_vector))
-        self.reward = int(self.rng.uniform() < self.current_mean) # essentially Bernuli with p = current_mean
+        context_vector = np.asarray(context_vector)
+        self.current_mean = np.dot(self.theta, context_vector)
+        self.reward = self.base_arm.pull(self.current_mean, size=size)
+        # self.reward = int(self.rng.uniform() < self.current_mean) # essentially Bernuli with p = current_mean
         return self.reward # 0 or 1 
 
 
