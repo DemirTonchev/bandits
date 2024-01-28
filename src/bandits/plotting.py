b0a5bbf8c8d822a20fc6d344e6b819ac9cc5128f
Demir Tonchev <tonchev.demir@gmail.com>
2024-01-28 22:46:44 +0200

    TS analitical probs

--- src/bandits/plotting.py
+++ src/bandits/plotting.py
@@ -3,41 +3,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import beta
 import scipy
+from math import prod
 
-def plot_instant_regret(data, title = None, label = None, ax = None, **kwargs):
+def _plot_regrets_over_time(data, y, title = None, label = None, ax = None, **kwargs):
     if ax is None:
         fig, ax = plt.subplots()
-    ax=sns.lineplot(x='t', y='instant_regret', data = data, estimator='mean',
-                 ci=None, label = label, ax=ax, **kwargs)
+    ax = sns.lineplot(x='t', y=y, data = data,
+                      estimator='mean',
+                      errorbar=None,
+                      label=label,
+                      ax=ax,
+                      **kwargs)
     if title is not None:
         ax.set_title(title)
+    return ax
+
+def plot_instant_regret(data, title = None, label = None, ax = None, **kwargs):
+    ax = _plot_regrets_over_time(data, y='instant_regret', title=title, label=label, ax=ax, **kwargs)
     return ax
 
 def plot_cumulative_regret(data, title = None, label = None, ax = None, **kwargs):
-    if ax is None:
-        fig, ax = plt.subplots()
-    ax=sns.lineplot(x='t', y='cumulative_regret', data = data, estimator='mean',
-                 ci=None, label = label, ax=ax, **kwargs)
-    if title is not None:
-        ax.set_title(title)
+    ax = _plot_regrets_over_time(data, y='cumulative_regret', title=title, label=label, ax=ax, **kwargs)
     return ax
 
-X = np.linspace(0, 1, 1000)
 def plot_TS_priors(prior_history, pulls_history, t=0, ax=None):
+    X = np.linspace(0, 1, 1000)
     if ax is None:
         fig, ax = plt.subplots()
-    for k, params in enumerate(prior_history.loc[t].values):
+    for k, params in enumerate(prior_history[t]):
         ax.plot(X, beta(*params).pdf(X), label = f'arm {k} beta({params[0]},{params[1]})')
     ax.legend(loc='best')
-    ax.set_title(f't={t}   ' + '; '.join(['Arm {}: {:.0f}'.format(k, pull) for k, pull in enumerate(pulls_history.loc[t].values)]) )
+    ax.set_title(f't={t}   ' + '; '.join(['Arm {}: {:.0f}'.format(k, pull) for k, pull in enumerate(pulls_history[t])]) )
     return ax
 
-def TS_probs(prior_history, t):
-    a1, a2,a3 = [beta(*params) for params in prior_history.loc[t].values]
+def TS_probs3(prior_history: list[list[np.ndarray]], t: int):
+    """Analitical solutions of the probability that each arm is the best at time t.
+    prior_history - list of list with beta params at each t. eg
+    at t=0 the list of beta params looks like 
+    [array([1, 1]), array([1, 1]), array([1, 1])]
+    """
+    
+    a1, a2, a3 = [beta(*params) for params in prior_history[t]]
     f1 = lambda x: a1.pdf(x)*a2.cdf(x)*a3.cdf(x) # prob of max for a_1
     f2 = lambda x: a2.pdf(x)*a1.cdf(x)*a3.cdf(x) # prob of max for a_2
     f3 = lambda x: a3.pdf(x)*a1.cdf(x)*a2.cdf(x) # prob of max for a_3
     return [scipy.integrate.quad(f,0,1)[0] for f in [f1,f2,f3]]
+
+def TS_probs(prior_history: list[list[np.ndarray]], t: int):
+    """Analitical solutions of the probability that each arm is the best at time t.
+    Should work for any number of arms
+    prior_history - list of list with beta params at each t. eg
+    at t=0 the list of beta params looks like 
+    [array([1, 1]), array([1, 1]), array([1, 1])]
+    """
+    # I dont know if this is beautiful or crazy ugly 
+    def _prod(rest):
+        def __prod(x):
+            return prod([rv.cdf(x) for rv in rest])
+        return __prod
+    def _prob(rv ,rest):
+        def prob(x):
+            return rv.pdf(x)*_prod(rest)(x)
+        return prob
+    #--------
+    rvs = [beta(*params) for params in prior_history[t]]
+    fs = []
+    for i in range(len(rvs)):
+        rv = rvs[i]
+        rest = [rvs[j] for j in range(len(rvs)) if j!=i]
+        fs.append(_prob(rv,rest))
+    return [scipy.integrate.quad(f,0,1)[0] for f in fs]
 
 def plot_ucb(estimated_means, ucbs, pulls_data, thetas, t=0, ax = None, ylim = None):
     arms = np.array(range(len(estimated_means.columns)))
