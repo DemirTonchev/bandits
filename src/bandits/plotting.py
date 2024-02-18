19bd62a3a2ebb591bdd9ea5180f6b5d7db24a164
Demir Tonchev <tonchev.demir@gmail.com>
2024-02-18 23:53:05 +0200

    ts plotting functions now just expect  ts_bandit as param

--- src/bandits/plotting.py
+++ src/bandits/plotting.py
@@ -26,14 +26,14 @@ def plot_cumulative_regret(data, title = None, label = None, ax = None, **kwargs
     ax = _plot_regrets_over_time(data, y='cumulative_regret', title=title, label=label, ax=ax, **kwargs)
     return ax
 
-def plot_TS_priors(prior_history, pulls_history, t=0, ax=None):
+def plot_TS_priors(ts_policy, t=0, ax=None):
     X = np.linspace(0, 1, 1000)
     if ax is None:
-        fig, ax = plt.subplots()
-    for k, params in enumerate(prior_history[t]):
+        _, ax = plt.subplots()
+    for k, params in enumerate(ts_policy.prior_data_history[t]):
         ax.plot(X, beta(*params).pdf(X), label = f'arm {k} beta({params[0]},{params[1]})')
     ax.legend(loc='best')
-    ax.set_title(f't={t}   ' + '; '.join(['Arm {}: {:.0f}'.format(k, pull) for k, pull in enumerate(pulls_history[t])]) )
+    ax.set_title(f't={t:<5}' + '; '.join([f'Arm {k}: {pull}' for k, pull in enumerate(ts_policy.pulls_history[t])]))
     return ax
 
 def TS_probs3(prior_history: list[list[np.ndarray]], t: int):
@@ -49,7 +49,7 @@ def TS_probs3(prior_history: list[list[np.ndarray]], t: int):
     f3 = lambda x: a3.pdf(x)*a1.cdf(x)*a2.cdf(x) # prob of max for a_3
     return [scipy.integrate.quad(f,0,1)[0] for f in [f1,f2,f3]]
 
-def TS_probs(prior_history: list[np.ndarray]):
+def TS_probs(ts_policy, t: int):
     """Analitical solutions of the probability that each arm is the best at time t.
     Should work for any number of arms
     prior_history - list of list with beta params at each t. eg
@@ -58,14 +58,14 @@ def TS_probs(prior_history: list[np.ndarray]):
 
     Returns the probability of each arm being the max
     """
-    # I dont know if this is beautiful or crazy ugly 
+    
     def _prob(rv, rest):
         #  we want a f of x for the integration
         def prob(x):
             return rv.pdf(x)*prod([rv_.cdf(x) for rv_ in rest])
         return prob
     #--------
-    rvs = [beta(*params) for params in prior_history]
+    rvs = [beta(*params) for params in ts_policy.prior_data_history[t]]
     fs = []
     for i in range(len(rvs)):
         rv = rvs[i]
@@ -90,5 +90,5 @@ def plot_ucb(estimated_means, ucbs, pulls_data, thetas, t=0, ax = None, ylim = N
             element.set_marker('_')
             element.set_markersize(20)
     ax.set_xticks(arms)
-    ax.set_title(f't={t}   ' + '; '.join(['Arm {}: {:.0f}'.format(k, pull) for k, pull in enumerate(pulls_data.loc[t].values)]) )
+    ax.set_title(f't={t:<5}' + '; '.join([f'Arm {k}: {pull}' for k, pull in enumerate(pulls_data.loc[t].values)]) )
     return ax
